d2a4ba2c7c3aad7c602c2b8fcc4dc0ea52441968
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-20 23:24:37 +0800

    Added boxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,23 @@ const Index = () => {
 	return (
 		<>
 			<div className="h-screen w-full bg-topog relative overflow-x-hidden">
-				<Nav/>
+				<Nav />
+
+				<div className="h-[80%] w-full flex justify-center items-center">
+					<div className="w-fit h-fit flex justify-center items-center gap-x-7">
+						<div className="border-2 border-black w-36 h-40 shadow-forBox"></div>
+						<div className="flex flex-col gap-y-7">
+							<div className="border-2 border-black w-36 h-40 shadow-forBox"></div>
+							<div className="border-2 border-black w-36 h-40 shadow-forBox"></div>
+						</div>
+						<div className="flex flex-col gap-y-7">
+							<div className="border-2 border-black w-36 h-40 shadow-forBox"></div>
+							<div className="border-2 border-black w-36 h-40 shadow-forBox"></div>
+							<div className="border-2 border-black w-36 h-40 shadow-forBox"></div>
+
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       fontFamily: {
         'quicksand': ['Quicksand']
       },
+      boxShadow: {
+        'forBox': 'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;'
+      },
       colors: {
         'mygreen': '#588F62',
         'lightg': '#5AE676',
