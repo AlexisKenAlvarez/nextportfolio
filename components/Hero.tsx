a397aa30da1c7b2cb4c73d90b96bed22ca09e395
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-21 23:27:39 +0800

    Added About

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="flex h-[80%] w-full max-w-[1600px] mx-auto min-h-[550px] justify-center">
             <div className="w-full xl:w-[35%] lg:w-[50%]  h-[85%] font-raleway px-7 mt-5 text-center lg:text-left flex lg:items-center">
                 <div className="h-fit w-full lg:w-fit">
-                    <p className="text-mygreen font-medium text-md">Hey 👋, my name is</p>
+                    <p className="text-mygreen font-semibold text-md">Hey 👋, my name is</p>
                     <h1 className="font-black text-3xl md:text-5xl mt-3">Alexis Ken Alvarez</h1>
                     <h2 className="font-black text-2xl md:text-4xl mt-1 opacity-70 relative z-[-1]">Full stack developer</h2>
                     <h3 className="font-medium text-grey max-w-[400px] text-[16px] mx-auto lg:mx-0 mt-4">I’m a junior web developer from Philippines. I have a passion on building websites and designing UI/UX. I’m currently focusing on NextJS and also website responsiveness. I explore new lessons everyday to improve my skills.</h3>
