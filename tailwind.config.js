2d65480d966e850c81bfef972e8ac1bddb0fb41a
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-21 13:52:16 +0800

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         'courier': ['Courier Prime']
       },
       boxShadow: {
-        'forBox': 'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;'
+        'forBox': 'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px'
       },
       colors: {
         'mygreen': '#588F62',
