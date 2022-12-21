b9817d73507643a9383cdb7b9e6a3677c94dadea
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-21 13:22:32 +0800

    Added key

--- components/BoxGroup.tsx
+++ components/BoxGroup.tsx
@@ -9,14 +9,29 @@ interface BoxProp {
 const BoxGroup: FunctionComponent<BoxProp>  = (props) => {
     const { className } = props
     const Box2 = [
-		<Image alt="node" src="/techs/node.webp" width="79" height="79" id="node"/>,
-		<Image alt="react" src="/techs/react.webp" width="79" height="79" id="react" />
+        {
+            id: '1',
+            item: <Image alt="node" src="/techs/node.webp" width="79" height="79" id="node"/>,
+        },
+        {
+            id: '2',
+            item: <Image alt="react" src="/techs/react.webp" width="79" height="79" id="react" />
+        },
 	]
 
 	const Box3 = [
-		<Image alt="ts" src="/techs/ts.webp" width="75" height="75" id="js" />,
-		<Image alt="js" src="/techs/js.webp" width="75" height="75" id="js" />,
-		<Image alt="tailwind" src="/techs/tailwind.webp" width="79" height="79" id="tailwind" />
+        {
+            id: '1',
+            item: <Image alt="ts" src="/techs/ts.webp" width="75" height="75" id="js" />,
+        },
+        {
+            id: '2',
+            item: <Image alt="js" src="/techs/js.webp" width="75" height="75" id="js" />,
+        },
+        {
+            id: '3',
+            item: <Image alt="tailwind" src="/techs/tailwind.webp" width="79" height="79" id="tailwind" />
+        },
 	]
     return (
         <div className={`w-fit h-fit flex justify-center items-center gap-x-7 ${className}`}>
@@ -26,8 +41,8 @@ const BoxGroup: FunctionComponent<BoxProp>  = (props) => {
             <div className="flex flex-col gap-y-7">
                 {Box2.map(items => {
                     return (
-                        <Box key={items.key}>
-                            {items}
+                        <Box key={items.id}>
+                            {items.item}
                         </Box>
                     )
                 })}
@@ -35,8 +50,8 @@ const BoxGroup: FunctionComponent<BoxProp>  = (props) => {
             <div className="flex flex-col gap-y-7">
                 {Box3.map(items => {
                     return (
-                        <Box key={items.key}>
-                            {items}
+                        <Box key={items.id}>
+                            {items.item}
                         </Box>
                     )
                 })}
