cc601fa0cd025fa4acccc7a799acfe4cf590f151
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-30 23:57:59 +0800

    changed about background

--- components/About.tsx
+++ components/About.tsx
@@ -15,14 +15,14 @@ const About: FunctionComponent = () => {
                         <h2 className="text-2xl font-raleway font-semibold hidden lg:block">Years of experience</h2>
                     </div>
                     <div className="sm:flex sm:gap-3">
-                        <div className="border-[1px] border-black shadow-forBox h-fit flex flex-col gap-y-10 p-5 py-10 mt-5 w-full max-w-[360px] sm:max-w-[1600px] xl:flex-row lg:pl-14 xl:gap-x-16 2xl:gap-x-32">
+                        <div className="border-[1px] border-black shadow-forBox h-fit flex flex-col gap-y-10 p-5 pb-[2.3rem] dark:pb-[1.3rem] mt-5 w-full max-w-[360px] sm:max-w-[1600px] xl:flex-row lg:pl-14 xl:gap-x-16 2xl:gap-x-32 dark:border-darkwhite dark:border-8 dark:bg-[#DCDCDC] dark:shadow-dark border-box">
 
-                            <div className="font-raleway font-medium">
+                            <div className="font-raleway font-medium mt-6">
                                 {info1.map(details => {
                                     return (
                                         <div key={details.id} className="flex mb-3 xl:mb-[35px]">
-                                            <p className="w-[5.8rem] opacity-70 dark:text-white">{details.label}: </p>
-                                            <p className="ml-3 text-mygreen dark:text-lightg">{details.value}</p>
+                                            <p className="w-[5.8rem] opacity-70 dark:opacity-100 font-bold dark:text-[#649f5b]">{details.label}: </p>
+                                            <p className="ml-3 text-mygreen dark:text-black">{details.value}</p>
                                         </div>
                                     )
                                 })
@@ -30,12 +30,12 @@ const About: FunctionComponent = () => {
 
                             </div>
 
-                            <div className="font-raleway font-medium">
+                            <div className="font-raleway font-medium mt-6">
                                 {info2.map(details => {
                                     return (
                                         <div key={details.id} className="flex mb-3 xl:mb-[35px]">
-                                            <p className="w-[7rem] opacity-70 dark:text-white">{details.label}: </p>
-                                            <p className="ml-3 text-mygreen w-[12rem] dark:text-lightg">{details.value}</p>
+                                            <p className="w-[7rem] opacity-70 dark:text-[#649f5b] dark:opacity-100 font-bold">{details.label}: </p>
+                                            <p className="ml-3 text-mygreen w-[12rem] dark:text-black">{details.value}</p>
                                         </div>
                                     )
                                 })
@@ -44,15 +44,15 @@ const About: FunctionComponent = () => {
 
                         </div>
                         <div className="flex flex-col sm:flex-col-reverse lg:flex-col">
-                            <h2 className="text-2xl font-raleway font-semibold lg:hidden mt-10 sm:text-right sm:w-[11rem]">Years of experience</h2>
+                            <h2 className="text-2xl font-raleway font-semibold lg:hidden mt-10 sm:text-right sm:w-[11rem] dark:text-white">Years of experience</h2>
                             <div className="flex gap-x-2 font-raleway text-center mt-5 sm:flex-col sm:gap-y-3">
-                                <div className="border-[1px] border-black shadow-forBox h-fit py-10 w-[11rem]">
-                                    <h1 className="text-5xl mb-7 font-semibold text-mygreen dark:text-lightg">3+</h1>
-                                    <p className='dark:text-white'>HTML, CSS, JS</p>
+                                <div className="border-[1px] border-black shadow-forBox h-fit py-10 w-[11rem] dark:py-[2.1rem] dark:border-darkwhite dark:border-8 dark:bg-[#DCDCDC] dark:shadow-dark">
+                                    <h1 className="text-5xl mb-7 font-semibold text-mygreen dark:text-[#649f5b]">3+</h1>
+                                    <p className='dark:text-black font-semibold text-grey'>HTML, CSS, JS</p>
                                 </div>
-                                <div className="border-[1px] border-black shadow-forBox h-fit py-10 w-[11rem]">
-                                    <h1 className="text-5xl mb-7 font-semibold text-mygreen dark:text-lightg">1+</h1>
-                                    <p className='dark:text-white'>React/NextJS</p>
+                                <div className="border-[1px] border-black shadow-forBox h-fit py-10 w-[11rem] dark:py-[2.1rem] dark:border-darkwhite dark:border-8 dark:bg-[#DCDCDC] dark:shadow-dark">
+                                    <h1 className="text-5xl mb-7 font-semibold text-mygreen dark:text-[#649f5b]">1+</h1>
+                                    <p className='dark:text-black  font-semibold text-grey'>React/NextJS</p>
                                 </div>
                             </div>
                         </div>
