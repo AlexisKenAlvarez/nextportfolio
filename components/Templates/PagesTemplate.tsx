c5a21fa058c32c0c79cb94678bdc02e249e193ef
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-01-30 15:55:43 +0800

    Fixed dark mode and lista image

--- components/Templates/PagesTemplate.tsx
+++ components/Templates/PagesTemplate.tsx
@@ -12,7 +12,7 @@ const PagesTemplate: FunctionComponent<props> = ({ image, alt }) => {
 
     return (
         <div className='w-full h-auto md:mb-16 mb-12 relative'>
-            <div className="w-full md:h-[2rem] h-[1.7rem] bg-[#EAEAEA] rounded-tl-lg rounded-tr-lg">
+            <div className="w-full md:h-[2rem] h-[1.7rem] bg-[#EAEAEA] dark:bg-[#2E2E2E] rounded-tl-lg rounded-tr-lg">
                 <div className='h-full w-[3rem] flex justify-around items-center ml-3'>
                     <div className="w-2 h-2 rounded-full bg-[#A7A7A7]"></div>
                     <div className="w-2 h-2 rounded-full bg-[#A7A7A7]"></div>
